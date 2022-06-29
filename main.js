05b7a1b654ebe8ded8d59c0f47d820d26b26a2c9
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2022-06-29 16:19:45 -0400

    Setup search bar/ connected to API

---
+++ main.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("form")
+const search = document.getElementById("search")
+const results = document.getElementById("results")
+const api = '2904de71'
+
+
+
+form.addEventListener('submit', e =>{
+    e.preventDefault()
+    query = search.value
+    searchMovie()
+})
+
+
+
+function searchMovie(query){
+    fetch(`https://www.omdbapi.com/?s=${query}&apikey=${api}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    });
+}
+

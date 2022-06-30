6c009bf7a6e2bb9a9eee3b5cfcf82f53837ec8f4
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2022-06-30 16:00:12 -0400

    Fetched movieID's

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const searchInput = document.getElementById("search")
 const api = '2904de71'
 let movieList = document.getElementById("movie-list")
 let movieTitle = document.getElementById("movie-title")
+let queryId = ""
+
 
 form.addEventListener('submit', e =>{
     e.preventDefault()
@@ -16,18 +18,30 @@ function searchMovie(query){
     .then(response => response.json())
     .then(data => {
         movieList = ""
-
         for (let i = 0; i < query.length; i++) {
             let movieItems = data.Search[i]
             movieTitle.innerHTML += `
-            <h2 class="title">${movieItems.Title}</h2>
+            <div class="movie-container">
             <img src="${movieItems.Poster}" class="poster">
+            <h2 class="title">${movieItems.Title}</h2>
             <p class="year">${movieItems.Year}</p>
+            
+            </div>
             `
             console.log(movieItems)
+            let movieId = movieItems.imdbID
+
+            fetch(`https://www.omdbapi.com/?i=${movieId}&apikey=${api}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+            })
+
           }
-          
-    });
+        })
 }
+
+
+
 
 
